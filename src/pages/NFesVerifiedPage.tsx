a0b69a0f4630273fe3bda6928e5f54f21fa051cb
MarceloHenrique7 <marcelohenriquex77@gmail.com>
2024-10-15 00:52:15 -0300

    fix

--- src/pages/NFesVerifiedPage.tsx
+++ src/pages/NFesVerifiedPage.tsx
@@ -10,7 +10,7 @@ import {
     DialogTitle,
     DialogTrigger,
  } from "../components/ui/dialog";
-import { INFe, Product } from "../types";
+import { Product } from "../types";
 import { ScrollArea } from "../components/ui/scroll-area";
 
 
