0b35e94275c818379f857629d27703d589346b44
MarceloHenrique7 <marcelohenriquex77@gmail.com>
2024-10-15 01:03:29 -0300

    att

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,9 +10,9 @@ const Header = () => {
     return (
         <div className="text-center p-8 bg-slate-900 text-white flex justify-between items-center">
             <div>
-                <h1 className="font-bold text-3xl">
+                <Link to={"/"} className="font-bold text-3xl">
                     Conferência de NFs
-                </h1>
+                </Link>
             </div>
             <div className="space-x-5 font-bold">
                 <Link to={"/conf"} >
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,30 @@
+import { ArrowBigRight } from "lucide-react"
+import { Link } from "react-router-dom"
 
 
 function HomePage() {
 
   return (
     <>
-      <div>
-        <h1> Home Page </h1>
+      <div className="flex flex-col gap-10">
+        <div className="font-bold flex flex-col gap-5">
+          <h1 className="text-xl">Primeira Etapa</h1>
+          <Link to={"/registrar-xml"} className="flex items-center justify-between hover:bg-gray-800 hover:text-white transition-all 300-ms bg-gray-200 rounded-md p-2 inline-block">
+            Cadastrar Nota Fiscal com XML<ArrowBigRight/>
+          </Link>
+        </div>
+        <div className="font-bold flex flex-col gap-5">
+          <h1 className="text-xl">Segunda Etapa</h1>
+          <Link to={"/conf"} className="flex items-center justify-between hover:bg-gray-800 hover:text-white transition-all 300-ms bg-gray-200 rounded-md p-2 inline-block">
+            Buscar pela Nota Fiscal <ArrowBigRight/>
+          </Link>
+        </div>
+        <div className="font-bold flex flex-col gap-5">
+          <h1 className="text-xl">Terceira Etapa</h1>
+          <Link to={"/"} className="flex items-center justify-between hover:bg-gray-800 hover:text-white transition-all 300-ms bg-gray-200 rounded-md p-2 inline-block">
+            Verificar Produtos <ArrowBigRight/>
+          </Link>
+        </div>
       </div>
     </>
   )
